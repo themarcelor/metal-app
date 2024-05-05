4d67041b757b5a42b4d67c15b577b21962467fb1
Marcelo Costa <themarcelor@gmail.com>
2024-05-04 21:29:32 -0500

    Sample app / first metric + docker cmds + config files

---
+++ emissor-de-metricas-simples/main_bkp.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	instrument "go.opentelemetry.io/otel/metric"
+	otel_metric "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
+
+var meuContador otel_metric.Int64Counter
+
+func main() {
+	ctx := context.Background()
+
+	serviceName := "metal-app"
+	collectorAddress := "localhost:4317"
+	log.Printf("Establishing gRPC connection with %s...\n", collectorAddress)
+
+	dopts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+		grpc.WithTimeout(5 * time.Second),
+	}
+	conn, err := grpc.DialContext(ctx, collectorAddress, dopts...)
+	if err != nil {
+		log.Fatalf("ERROR: Unable to establish grpc connection to otel metrics collector: %w", err)
+	}
+
+	exporter, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithGRPCConn(conn))
+	if err != nil {
+		log.Fatalf("unable to create otel grpc exporter: %w", err)
+	}
+
+	var attrs []attribute.KeyValue
+	// Always include service_name by default
+	attrs = append(attrs, semconv.ServiceName(serviceName))
+
+	res, err := resource.New(ctx, resource.WithAttributes(
+		attrs...,
+	))
+	if err != nil {
+		log.Fatalf("failed to create resource: %w", err)
+	}
+
+	provider := metric.NewMeterProvider(
+		metric.WithResource(res),
+		metric.WithReader(metric.NewPeriodicReader(exporter)),
+	)
+	meter := provider.Meter("sre")
+
+	c, err := meter.Int64Counter("sre.terceiro_contador")
+	if err != nil {
+		log.Fatalf("unable to create counter metric", err)
+	}
+	meuContador = c
+
+	http.HandleFunc("/", HelloServer)
+	// leave http server running in a go routine (async)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var attrs = []attribute.KeyValue{
+		attribute.Key("operacao").String("oi"),
+	}
+	opt := instrument.WithAttributes(attrs...)
+
+	log.Print("emitindo metrica OTel...")
+	meuContador.Add(ctx, 1, opt)
+
+	fmt.Fprintf(w, "Olá, %s!", r.URL.Path[1:])
+}
